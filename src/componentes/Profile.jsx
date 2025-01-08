c5aeb49f9c430833e69c373304e9a2a9968a5f25
Haktor23 <hecburmar@alu.edu.gva.es>
2025-01-09 00:26:38 +0100

    Actualizar descripción

--- src/componentes/Profile.jsx
+++ src/componentes/Profile.jsx
@@ -4,15 +4,13 @@ function Profile() {
   return (
     <div className="info-personal">
       <h2 className="descripcion">
-        ¡Hola! Soy Hector, apasionado de la informática y la programación.
+        ¡Hola! Soy Hector, un apasionado de la informática y la programación.
         <br /><br />
-        Tras muchas horas de código y café, obtuve mi título de Desarrollador de Aplicaciones Web (DAW). <br />
-        <br />
-        Mi ordenador y yo estamos ansiosos por resolver problemas y enfrentar nuevos desafíos.
+        Después de dedicarme a aprender y perfeccionar mis habilidades, obtuve mi título de Desarrollador de Aplicaciones Web (DAW). Actualmente, estoy especializándome en Inteligencia Artificial y Big Data, áreas que me entusiasman por su potencial para transformar el mundo.
         <br /><br />
-        Si buscas a alguien con pasión y humor para tu equipo, ¡aquí estoy!
-        <br /> <br />
-        En este portafolio, mostraré mis habilidades actuales y las que estoy aprendiendo, para que puedas ver todo lo que puedo ofrecer.
+        Trabajo como desarrollador de software en Aplisein, donde disfruto resolviendo problemas y enfrentándome a desafíos únicos cada día. Mi ordenador y yo estamos siempre listos para aprender y crecer.
+        <br /><br />
+        En este portafolio encontrarás mis habilidades actuales y los conocimientos que sigo adquiriendo, para que puedas descubrir todo lo que puedo ofrecer.
       </h2>
     </div>
   );
